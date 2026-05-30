a944b161aec9e5faa31bfbcc91706742693fbd2d
k.tamura <k.tamura@kitsune-creative.studio>
2026-05-30 18:17:21 +0900

    feat: Skills途中まで作成、card-badgeから実装を進める.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import Header from "./components/Header"
 import ScrollToTop from "./components/ScrollToTop"
 import { useTheme } from "./hooks/useTheme"
 import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Skills from "./components/Skills"
 
 function HomePage(){
   return(
     <>
+      <Hero/>
+      <About/>
+      <Skills/>
     </>
   )
 }
